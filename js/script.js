e6e42c2fb8dfc7c2ea74fa4c7389e3e7de7378c2
neocodech <michi.wieland@hotmail.com>
2016-03-05 17:14:40 +0100

    make travis happy

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-jQuery(document).ready(function( $ ) {
+jQuery( document ).ready( function( $ ) {
 	// Header Scroll
-	$(window).on('scroll', function() {
-		var scroll = $(window).scrollTop();
+	$( window ).on( 'scroll' , function() {
+		var scroll = $( window ).scrollTop();
 
-		if (scroll >= 50) {
-			$('#header').addClass('fixed');
+		if ( scroll >= 50 ) {
+			$( '#header' ).addClass( 'fixed' );
 		} else {
-			$('#header').removeClass('fixed');
+			$( '#header' ).removeClass( 'fixed' );
 		}
 	});
 });
